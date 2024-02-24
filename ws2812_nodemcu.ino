5197e14bda8d8cc3caf602ddbda94638ea1bd246
ChaosPrometheus <79803528+ChaosPrometheus@users.noreply.github.com>
2024-02-24 21:52:52 +0500

    Update ws2812_nodemcu.ino

--- ws2812_nodemcu.ino
+++ ws2812_nodemcu.ino
@@ -10,31 +10,31 @@ Adafruit_NeoPixel matrix(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 void setup() {
   Serial.begin(9600);
   matrix.begin();
-  matrix.show(); // Initialize all pixels to 'off'
+  matrix.show(); 
 }
 
 void loop() {
   if (Serial.available() > 0) {
-    String command = Serial.readStringUntil(' '); // Read command until first space
+    String command = Serial.readStringUntil(' '); 
     if (command == "/on") {
-      int x = Serial.parseInt(); // Read x coordinate
-      int y = Serial.parseInt(); // Read y coordinate
-      int r = Serial.parseInt(); // Read red color intensity
-      int g = Serial.parseInt(); // Read green color intensity
-      int b = Serial.parseInt(); // Read blue color intensity
-      int brightness = Serial.parseInt(); // Read brightness
-      // Set color and brightness of the specified LED
+      int x = Serial.parseInt(); 
+      int y = Serial.parseInt(); 
+      int r = Serial.parseInt();
+      int g = Serial.parseInt(); 
+      int b = Serial.parseInt(); 
+      int brightness = Serial.parseInt();
+
       if (x >= 0 && x < LED_WIDTH && y >= 0 && y < LED_HEIGHT) {
-        matrix.setPixelColor(y * LED_WIDTH + x, matrix.Color(r, g, b)); // Set color
-        matrix.setBrightness(brightness); // Set brightness
+        matrix.setPixelColor(y * LED_WIDTH + x, matrix.Color(r, g, b));
+        matrix.setBrightness(brightness); 
         matrix.show();
       }
     } else if (command == "/off") {
-      int x = Serial.parseInt(); // Read x coordinate
-      int y = Serial.parseInt(); // Read y coordinate
+      int x = Serial.parseInt(); 
+      int y = Serial.parseInt(); 
       // Turn off the specified LED
       if (x >= 0 && x < LED_WIDTH && y >= 0 && y < LED_HEIGHT) {
-        matrix.setPixelColor(y * LED_WIDTH + x, matrix.Color(0, 0, 0)); // Turn off LED
+        matrix.setPixelColor(y * LED_WIDTH + x, matrix.Color(0, 0, 0)); 
         matrix.show();
       }
     }
