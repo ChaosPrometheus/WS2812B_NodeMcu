9a4fb3040903eaa8c22c243a598740c325888f4e
ChaosPrometheus <79803528+ChaosPrometheus@users.noreply.github.com>
2024-10-09 14:23:51 +0500

    Update ws2812_nodemcu.ino

--- ws2812_nodemcu.ino
+++ ws2812_nodemcu.ino
@@ -1,11 +1,21 @@
 #include <Adafruit_NeoPixel.h>
 
 #define LED_PIN     D4
-#define LED_WIDTH   2
-#define LED_HEIGHT  2
+#define LED_WIDTH   42
+#define LED_HEIGHT  8
 #define LED_COUNT   (LED_WIDTH * LED_HEIGHT)
 
 Adafruit_NeoPixel matrix(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+
+int getPixelIndex(int x, int y) {
+  x -= 1;
+  y -= 1;
+  if (y % 2 == 0) {
+    return y * LED_WIDTH + x;
+  } else {
+    return y * LED_WIDTH + (LED_WIDTH - 1 - x);
+  }
+}
 
 void setup() {
   Serial.begin(9600);
@@ -23,22 +33,18 @@ void loop() {
       int g = Serial.parseInt(); 
       int b = Serial.parseInt(); 
       int brightness = Serial.parseInt();
-
-      if (x >= 0 && x < LED_WIDTH && y >= 0 && y < LED_HEIGHT) {
-        matrix.setPixelColor(y * LED_WIDTH + x, matrix.Color(r, g, b));
+      if (x > 0 && x <= LED_WIDTH && y > 0 && y <= LED_HEIGHT) {
+        matrix.setPixelColor(getPixelIndex(x, y), matrix.Color(r, g, b));
         matrix.setBrightness(brightness); 
         matrix.show();
       }
     } else if (command == "/off") {
       int x = Serial.parseInt(); 
       int y = Serial.parseInt(); 
-      // Turn off the specified LED
-      if (x >= 0 && x < LED_WIDTH && y >= 0 && y < LED_HEIGHT) {
-        matrix.setPixelColor(y * LED_WIDTH + x, matrix.Color(0, 0, 0)); 
+      if (x > 0 && x <= LED_WIDTH && y > 0 && y <= LED_HEIGHT) {
+        matrix.setPixelColor(getPixelIndex(x, y), matrix.Color(0, 0, 0)); 
         matrix.show();
       }
     }
   }
-
-  
 }
